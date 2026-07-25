70f1333f75c523e0b24bff4ba8a5a5cce70aad2b
m1k-2y <gssg1121@gmail.com>
2026-07-26 06:49:51 +0900

    Add attack step edge case tests

--- tests/test_robustness.py
+++ tests/test_robustness.py
@@ -33,3 +33,41 @@ def test_measure_attack_step_disconnected_graph():
     assert step['giant_component_size'] == 3
     assert step['giant_component_ratio'] == 0.5
     assert step['removed_item'] == 3
+
+def test_measure_attack_step_empty_graph():
+
+    graph = nx.path_graph(4)
+
+    initial_node_count = graph.number_of_nodes()
+    graph.remove_nodes_from(list(graph.nodes))
+    removed_fraction = 1.0
+    removed_item = 3
+
+    step = measure_attack_step(graph, initial_node_count, removed_fraction, removed_item)
+
+    assert step['removed_fraction'] == 1.0
+    assert step['remaining_nodes'] == 0
+    assert step['component_count'] == 0
+    assert step['giant_component_size'] == 0
+    assert step['giant_component_ratio'] == 0.0
+    assert step['removed_item'] == removed_item
+
+def test_measure_attack_step_single_node_graph():
+
+    graph = nx.path_graph(4)
+    initial_node_count = graph.number_of_nodes()
+
+    for i in range(3):
+        graph.remove_node(i)
+
+    removed_fraction = 3 / initial_node_count
+    removed_item = 2
+
+    step = measure_attack_step(graph, initial_node_count, removed_fraction, removed_item)
+
+    assert step['removed_fraction'] == removed_fraction
+    assert step['remaining_nodes'] == 1
+    assert step['component_count'] == 1
+    assert step['giant_component_size'] == 1
+    assert step['giant_component_ratio'] == 0.25
+    assert step['removed_item'] == removed_item
